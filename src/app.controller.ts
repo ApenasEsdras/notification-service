e6c5b864aea95556b5515d09189d92d90d3a7180
Apenas Esdras <93005998+ApenasEsdras@users.noreply.github.com>
2022-12-15 11:20:10 -0300

    atualização

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,19 +2,22 @@ import { Controller, Get } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
 import { randomUUID } from 'crypto';
 import { PrismaService } from './prisma.service';
+// import { Body, Post } from '@nestjs/common/decorators';
+// import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  List() {
     return this.prisma.notification.findMany();
   }
 
   @Post()
+  //faz o retorno de mina postagem
   async create(@Body() body: any) {
-    const {recipientId, content, category} = body
+    const { recipientId, content, category } = body;
 
     await this.prisma.notification.create({
       data: {
